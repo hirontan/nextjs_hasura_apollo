38008776e6c0796d180d4e6c2934bc109671831a
hirontan <hirontan@sora.flights>
2021-09-27 18:56:34 +0900

    [Update] タイトルをStringの型とする

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,9 @@
 import { ReactNode, VFC } from "react";
 import Head from "next/head";
 import Link from "next/link";
+
+// タイトルをStringの型とする
+interface Props {
+  children: ReactNode
+  title: string
+}
