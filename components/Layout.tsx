51922b85dea5bcb3ebb4366202001f8929ff9d92
hirontan <hirontan@sora.flights>
2021-09-27 19:06:07 +0900

    [Update] ルートのリンクを設定

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -17,6 +17,22 @@ export const Layout: VFC<Props> = ({
       <Head>
         <title>{title}</title>
       </Head>
+      <header>
+        <nav className="bg-gray-800 w-screen">
+          <div className="flex items-center pl-8 h-14">
+            <div className="flex space-x-4">
+              <Link href="/">
+                <a
+                  data-testid="home-nav"
+                  className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded"
+                >
+                  Home
+                </a>
+              </Link>
+            </div>
+          </div>
+        </nav>
+      </header>
     </div>
   )
 }
