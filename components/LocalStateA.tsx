8503bb26adffde7aed447c745f2cf6db27faeaed
hirontan <hirontan@sora.flights>
2021-09-28 23:09:08 +0900

    [Add] LocalStateA.tsx

---
+++ components/LocalStateA.tsx
@@ -0,0 +1,7 @@
+import { ChangeEvent, FormEvent, useState, VFC } from "react";
+import { todoVar } from "../cache";
+import { useReactiveVar } from "@apollo/client";
+import Link from "next/link";
+
+export const LocalStateA: VFC = () => {
+}
