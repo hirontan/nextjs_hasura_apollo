de98530ebda604adb8d32f7fb1e3911fca44d915
hirontan <hirontan@sora.flights>
2021-09-28 23:24:03 +0900

    [Update] Stateを追加できるFunctional Component

--- components/LocalStateA.tsx
+++ components/LocalStateA.tsx
@@ -4,4 +4,35 @@ import { useReactiveVar } from "@apollo/client";
 import Link from "next/link";
 
 export const LocalStateA: VFC = () => {
+  const [input, setInput] = useState('')
+  const todos = useReactiveVar(todoVar)
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault() // リロードしないように
+    todoVar([...todoVar(), { title: input }]) // 現在の配列の値の最後に値を追加する
+    setInput('')
+  }
+
+
+  return (<>
+    <p className="mb-3 font-bold">makeVar</p>
+    {todos?.map((task, index) => {
+      return (
+        <p className="mb-3 y-1" key={index}>
+          {task.title}
+        </p>
+      )
+    })}
+    <form
+      className="flex flex-col justify-center items-center"
+      onSubmit={handleSubmit}
+    >
+      <input className="mb-3 px-3 py-2 border border-gray-300" placeholder="New task ?" value={input} onChange={(e: ChangeEvent<HTMLInputElement>) => setInput(e.target.value)} />
+      <button disabled={!input} className="disabled:opacity-40 mb-3 py-1 text-white bg-indigo-600 hover:bg-indigo-700 rounded-2xl focus:outline-none" type="submit">
+        Add new state
+      </button>
+    </form>
+    <Link href="/local-state-b">
+      <a>Next</a>
+    </Link>
+  </>)
 }
