63b25f6a94785519f66f1e1909c37115c708744c
hirontan <hirontan@sora.flights>
2021-09-28 17:10:19 +0900

    [Update] footerを作成

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -76,6 +76,17 @@ export const Layout: VFC<Props> = ({
       <main className="flex flex-1 flex-col justify-center items-center w-screen">
         {children}
       </main>
+      <footer className="w-full h-12 flex justify-center items-center border-t">
+        <a
+          className="flex items-center"
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
+        </a>
+      </footer>
     </div>
   )
 }
