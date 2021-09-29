f9167d15ba349b83ceb01defb57136570081c1f5
hirontan <hirontan@sora.flights>
2021-09-29 18:45:56 +0900

    [Add] Apollo Clientライブラリ

---
+++ lib/apolloClient.ts
@@ -0,0 +1,29 @@
+// 参考: https://github.com/vercel/next.js/blob/canary/examples/with-apollo/lib/apolloClient.js
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  NormalizedCacheObject,
+} from '@apollo/client'
+import 'cross-fetch/polyfill'
+
+let apolloClient: ApolloClient<NormalizedCacheObject> | undefined
+
+const createApolloClient = () => {
+  return new ApolloClient({
+    ssrMode: typeof window === 'undefined', // window ブラウザで実行している
+    link: new HttpLink({ uri: process.env.APOLLO_URL }),
+    cache: new InMemoryCache(),
+  })
+}
+
+export const initializeApollo = (initialState = null) => {
+  const _apolloClient = apolloClient ?? createApolloClient()
+
+  // For SSG and SSR always create a new Apollo Client
+  if (typeof window === 'undefined') return _apolloClient
+  // Create the Apollo Client once in the client
+  if (!apolloClient) apolloClient = _apolloClient
+
+  return _apolloClient
+}
