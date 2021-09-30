79594e585fca559975c881c906af54fc75ab800e
hirontan <hirontan@sora.flights>
2021-09-30 23:11:31 +0900

    [Update] エラーが起こった場合のメッセージ表示

--- pages/hasura-main.tsx
+++ pages/hasura-main.tsx
@@ -4,3 +4,15 @@ import { useQuery } from '@apollo/client'
 import { GET_USERS } from '../queries/queries'
 import { GetUsersQuery } from '../types/generated/graphql'
 import { Layout } from '../components/Layout'
+
+const FetchMain: VFC = () => {
+  const { data, error } = useQuery<GetUsersQuery>(GET_USERS)
+  if (error)
+    return (
+      <Layout title="Hasura fetchPolicy">
+        <p>Error: {error.message}</p>
+      </Layout>
+    )
+}
+
+export default FetchMain
