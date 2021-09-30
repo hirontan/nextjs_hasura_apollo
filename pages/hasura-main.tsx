2f1f35057ae4bdb2b44d9ef7a6ea8bd4d4b98032
hirontan <hirontan@sora.flights>
2021-09-30 23:12:58 +0900

    [Update] エラーが起こらず正常だった場合の表示

--- pages/hasura-main.tsx
+++ pages/hasura-main.tsx
@@ -13,6 +13,22 @@ const FetchMain: VFC = () => {
         <p>Error: {error.message}</p>
       </Layout>
     )
+  return (
+    <Layout title="Hasura fetchPolicy">
+      <p className="mb-6 font-bold">Hasura main page</p>
+
+      {data?.users.map((user) => {
+        return (
+          <p className="my-1" key={user.id}>
+            {user.name}
+          </p>
+        )
+      })}
+      <Link href="/hasura-sub">
+        <a className="mt-6">Next</a>
+      </Link>
+    </Layout>
+  )
 }
 
 export default FetchMain
