1ea7c28293d7ffe039e0624305287ba7bbf73676
hirontan <hirontan@sora.flights>
2021-10-01 10:45:29 +0900

    [Update] FetchMainと同じ出力結果をコピー

--- pages/hasura-sub.tsx
+++ pages/hasura-sub.tsx
@@ -4,3 +4,22 @@ import { useQuery } from '@apollo/client'
 import { GET_USERS } from '../queries/queries'
 import { GetUsersQuery } from '../types/generated/graphql'
 import { Layout } from '../components/Layout'
+
+const FetchSub: VFC = () => {
+  const { data } = useQuery<GetUsersQuery>(GET_USERS)
+  return (
+    <Layout title="Hasura fetchPolicy read cache">
+      <p className="mb-6 font-bold">Hasura main page</p>
+      {console.log(data)}
+      {data?.users.map((user) => {
+        return (
+          <p className="my-1" key={user.id}>
+            {user.name}
+          </p>
+        )
+      })}
+    </Layout>
+  )
+}
+
+export default FetchSub
