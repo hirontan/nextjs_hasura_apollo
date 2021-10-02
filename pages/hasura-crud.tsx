5f811a05ba4eaa627e55b7584682971f4cea46b7
hirontan <hirontan@sora.flights>
2021-10-02 10:58:23 +0900

    [Update] ユーザ名・編集ボタン・削除ボタンを出力

--- pages/hasura-crud.tsx
+++ pages/hasura-crud.tsx
@@ -14,6 +14,7 @@ import {
   UpdateUserMutation
 } from '../types/generated/graphql'
 import { Layout } from '../components/Layout'
+import { UserItem } from '../components/UserItem'
 
 const HasuraCRUD: VFC = () => {
   const [editedUser, setEditedUser] = useState({id: '', name: ''})
@@ -85,6 +86,10 @@ const HasuraCRUD: VFC = () => {
   return (
     <Layout title="Hasura CRUD">
       <p className="mb-3 font-bold">Hasura CRUD</p>
+      <form
+        className="flex flex-col justify-center items-center"
+        onSubmit={handleSubmit}
+      >
         <input
           className="px-3 py-2 border border-gray-300"
           placeholder="New user ?"
@@ -101,7 +106,19 @@ const HasuraCRUD: VFC = () => {
           type="submit"
         >
           {editedUser.id ? 'Update' : 'Create'}
-      </button>
+        </button>
+      </form>
+
+      {data?.users.map((user) => {
+        return (
+          <UserItem
+            key={user.id}
+            user={user}
+            setEditedUser={setEditedUser}
+            delete_users_by_pk={delete_users_by_pk}
+          />
+        )
+      })}
     </Layout>
   )
 }
