09e3e53c84238af78a86fdafaf14ccfaa8f732e2
hirontan <hirontan@sora.flights>
2021-10-02 10:48:24 +0900

    [Update] ボタンの追加

--- pages/hasura-crud.tsx
+++ pages/hasura-crud.tsx
@@ -57,29 +57,51 @@ const HasuraCRUD: VFC = () => {
     e.preventDefault()
     if (editedUser.id) {
       try {
-        await update_users_by_pk({variables: {id: editedUser.id, name: editedUser.name}})
+        await update_users_by_pk({
+          variables: {
+            id: editedUser.id,
+            name: editedUser.name,
+          },
+        })
       } catch (err) {
         alert(err.message)
       }
-      setEditedUser({id: '', name: ''})
+      setEditedUser({ id: '', name: '' })
     } else {
       try {
-          await insert_users_one({
-            variables: {
-            name: editedUser.name
-          }
+        await insert_users_one({
+          variables: {
+            name: editedUser.name,
+          },
         })
-      } catch(err) {
+      } catch (err) {
         alert(err.message)
       }
-      setEditedUser({id: '', name: ''})
+      setEditedUser({ id: '', name: '' })
+    }
   }
 
   if (error) return <Layout title="Hasura CRUD">Error: { error.message }</Layout>
   return (
     <Layout title="Hasura CRUD">
       <p className="mb-3 font-bold">Hasura CRUD</p>
-      <form className="flex flex-col justify^center items-center" onSubmit={handleSubmit}></form>
+        <input
+          className="px-3 py-2 border border-gray-300"
+          placeholder="New user ?"
+          type="text"
+          value={editedUser.name}
+          onChange={(e) =>
+            setEditedUser({ ...editedUser, name: e.target.value })
+          }
+        />
+        <button
+          disabled={!editedUser.name}
+          className="disabled:opacity-40 my-3 py-1 px-3 text-white bg-indigo-600 hover:bg-indigo-700 rounded-2xl focus:outline-none"
+          data-testid="new"
+          type="submit"
+        >
+          {editedUser.id ? 'Update' : 'Create'}
+      </button>
     </Layout>
   )
 }
