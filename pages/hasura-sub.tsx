cb9352919144f2546a64c37fdf37c4ebafef6cfe
hirontan <hirontan@sora.flights>
2021-10-02 09:55:12 +0900

    [Update] キャッシュを読みに行く設定に変更

--- pages/hasura-sub.tsx
+++ pages/hasura-sub.tsx
@@ -1,14 +1,12 @@
 import { VFC } from 'react'
 import Link from 'next/link'
 import { useQuery } from '@apollo/client'
-import { GET_USERS } from '../queries/queries'
+import { GET_USERS_LOCAL } from '../queries/queries'
 import { GetUsersQuery } from '../types/generated/graphql'
 import { Layout } from '../components/Layout'
 
 const FetchSub: VFC = () => {
-  const { data } = useQuery<GetUsersQuery>(GET_USERS, {
-    fetchPolicy: 'network-only'
-  })
+  const { data } = useQuery<GetUsersQuery>(GET_USERS_LOCAL)
   return (
     <Layout title="Hasura fetchPolicy read cache">
       <p className="mb-6 font-bold">Hasura main page</p>
