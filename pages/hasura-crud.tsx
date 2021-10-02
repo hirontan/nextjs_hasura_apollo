3d52532f82984da99859756bf2611a83541d8f59
hirontan <hirontan@sora.flights>
2021-10-02 10:12:54 +0900

    [Update] Layoutのインポート

--- pages/hasura-crud.tsx
+++ pages/hasura-crud.tsx
@@ -6,9 +6,17 @@ import {
   DELETE_USER,
   UPDATE_USER
 } from '../queries/queries'
+// データ型
 import {
   GetUsersQuery,
   CreateUserMutation,
   DeleteUserMutation,
   UpdateUserMutation
 } from '../types/generated/graphql'
+import { Layout } from '../components/Layout'
+
+const HasuraCRUD: VFC = () => {
+  return(<Layout title="Hasura CRUD"></Layout>)
+}
+
+export default HasuraCRUD
