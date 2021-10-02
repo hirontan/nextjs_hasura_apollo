f64e0be0a96d68c86430dce7272d9233b47544dd
hirontan <hirontan@sora.flights>
2021-10-02 10:09:48 +0900

    [Update] Queryをインポート

--- pages/hasura-crud.tsx
+++ pages/hasura-crud.tsx
@@ -1,2 +1,14 @@
 import { VFC, useState, FormEvent } from 'react'
 import { useQuery, useMutation } from '@apollo/client'
+import {
+  GET_USERS,
+  CREATE_USER,
+  DELETE_USER,
+  UPDATE_USER
+} from '../queries/queries'
+import {
+  GetUsersQuery,
+  CreateUserMutation,
+  DeleteUserMutation,
+  UpdateUserMutation
+} from '../types/generated/graphql'
