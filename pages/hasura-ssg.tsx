d47469fc973adae2a4f5f3084303b55b8820fe3a
hirontan <hirontan@sora.flights>
2021-10-03 15:10:13 +0900

    [Update] ユーザがクリックされた場合のリンクを作成

--- pages/hasura-ssg.tsx
+++ pages/hasura-ssg.tsx
@@ -16,6 +16,15 @@ const HasuraSSG: VFC<Props> = ({ users }) => {
   return (
     <Layout title="Hasura SSG">
       <p className="mb-3 font-bold">SSG+ISR</p>
+      {users?.map((user) => {
+        return (
+          <Link key={user.id} href={`/users/${user.id}`}>
+            <a className="my-1 cursor-pointer" data-testid={`link-${user.id}`}>
+              {user.name}
+            </a>
+          </Link>
+        )
+      })}
     </Layout>
   )
 }
