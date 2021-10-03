52a7d4812127b777247f8c65cbd84f7ef864dcf2
hirontan <hirontan@sora.flights>
2021-10-03 15:07:06 +0900

    [Update] クエリの結果をdataで受け取る

--- pages/hasura-ssg.tsx
+++ pages/hasura-ssg.tsx
@@ -5,3 +5,14 @@ import { initializeApollo } from '../lib/apolloClient'
 import { GET_USERS } from '../queries/queries'
 import { GetUsersQuery, Users } from '../types/generated/graphql'
 import { Layout } from '../components/Layout'
+
+export const getStaticProps: GetStaticProps = async () => {
+  const apolloClient = initializeApollo()
+  const { data } = await apolloClient.query<GetUsersQuery>({
+    query: GET_USERS,
+  })
+  return {
+    props: { users: data.users },
+    revalidate: 1,
+  }
+}
