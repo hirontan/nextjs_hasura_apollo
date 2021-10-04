5b972d05faf735003ed4c3831097609996095077
hirontan <hirontan@sora.flights>
2021-10-04 18:38:30 +0900

    [Add] ユーザを作成するフォームのカスタムフックを作成する骨子

---
+++ hooks/useCreateForm.ts
@@ -0,0 +1,4 @@
+import { useState, useCallback, ChangeEvent, FormEvent } from 'react'
+import { useMutation } from '@apollo/client'
+import { CREATE_USER } from '../queries/queries'
+import { CreateUserMutation } from '../types/generated/graphql'
