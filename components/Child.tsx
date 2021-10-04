435bb0fec416dbf9e15c7eba7c0153d8f0f29ce2
hirontan <hirontan@sora.flights>
2021-10-04 18:59:48 +0900

    [Update] printMessageをボタンが押されたときに実行する

--- components/Child.tsx
+++ components/Child.tsx
@@ -5,4 +5,17 @@ interface Props {
   handleSubmit: (e: FormEvent<HTMLFormElement>) => Promise<void>
 }
 
-export const Child: VFC<Props> = ({ printMessage }) => {}
+export const Child: VFC<Props> = ({ printMessage }) => {
+  return (
+    <>
+      {console.log('Child rendered')}
+      <p>Child Component</p>
+      <button
+        className="my-3 py-1 px-3 text-white bg-indigo-600 hover:bg-indigo-700 rounded-2xl focus:outline-none"
+        onClick={printMessage}
+      >
+        click
+      </button>
+    </>
+  )
+}
