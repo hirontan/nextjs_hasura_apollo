5041e78cb4446368ee37f0c221968798ca99e74b
hirontan <hirontan@sora.flights>
2021-10-04 18:42:08 +0900

    [Update] const [insert_users_one]のコピー

--- hooks/useCreateForm.ts
+++ hooks/useCreateForm.ts
@@ -6,4 +6,16 @@ import { CreateUserMutation } from '../types/generated/graphql'
 export const useCreateForm = () => {
   const [text, setText] = useState('')
   const [username, setUsername] = useState('')
+  const [insert_users_one] = useMutation<CreateUserMutation>(CREATE_USER, {
+    update(cache, { data: { insert_users_one } }) {
+      const cacheId = cache.identify(insert_users_one) // cacheのIDを取得できる
+      cache.modify({
+        fields: {
+          users(existingUsers, { toReference }) {
+            return [toReference(cacheId, ...existingUsers)] // 配列の先頭にデータを追加する
+          },
+        },
+      })
+    },
+  })
 }
