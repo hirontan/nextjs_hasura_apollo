af677e32660d5fd10a142680edd86b1186bcacbb
hirontan <hirontan@sora.flights>
2021-10-06 17:32:44 +0900

    [Add] NavBarの遷移がただしくできているかテスト

---
+++ __tests__/NavBar.tsx
@@ -0,0 +1,11 @@
+// NavBarの遷移がただしくできているかテスト
+
+import { render, screen, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import userEvent from '@testing-library/user-event'
+import { getPage, initTestHelpers } from 'next-page-tester'
+import { setupServer } from 'msw/node'
+import { handlers } from '../mock/handlers'
+import 'setimmediate'
+
+initTestHelpers()
