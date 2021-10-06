365a0c29aa245efd76398bc6c7bc0d492a2e1319
hirontan <hirontan@sora.flights>
2021-10-06 17:30:02 +0900

    [Update] GetUserByIdのMock

--- mocks/handler.ts
+++ mocks/handler.ts
@@ -1,6 +1,6 @@
 import { graphql } from 'msw'
 
-export const handlers = {
+export const handlers = [
   graphql.query('GetUsers', (req, res, ctx) => {
     return res(
       ctx.data({
@@ -27,24 +27,43 @@ export const handlers = {
       })
     )
   }),
-  graphql.query('GetUserIDs', (req, res, ctx) => {
-    return res(
-      ctx.data({
-        users: [
-          {
+  graphql.query('GetUserById', (req, res, ctx) => {
+    const { id } = req.variables
+    if (id === 'aaaa1111-aa11-aa11-aa11-aaaaaa1111113') {
+      return res(
+        ctx.data({
+          users_by_pk: {
             __typename: 'users',
             id: 'aaaa1111-aa11-aa11-aa11-aaaaaa111111',
+            name: 'Test user A',
+            created_at: '2021-10-06T00:00:00.000000+00:00',
           },
-          {
+        })
+      )
+    }
+    if (id === 'bbbb1111-bb11-bb11-aa11-bbbbbb111111') {
+      return res(
+        ctx.data({
+          users_by_pk: {
             __typename: 'users',
             id: 'bbbb1111-bb11-bb11-aa11-bbbbbb111111',
+            name: 'Test user B',
+            created_at: '2021-10-06T00:00:00.000000+00:00',
           },
-          {
+        })
+      )
+    }
+    if (id === 'cccc1111-cc11-cc11-cc11-ccccccc111111') {
+      return res(
+        ctx.data({
+          users_by_pk: {
             __typename: 'users',
             id: 'cccc1111-cc11-cc11-cc11-ccccccc111111',
+            name: 'Test user C',
+            created_at: '2021-10-06T00:00:00.000000+00:00',
           },
-        ],
-      })
-    )
+        })
+      )
+    }
   }),
-}
+]
