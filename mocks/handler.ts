56084bbb660fcc5398e7b392f2b6a31456064fa6
hirontan <hirontan@sora.flights>
2021-10-06 17:28:13 +0900

    [Update] GetUserIDsのMock

--- mocks/handler.ts
+++ mocks/handler.ts
@@ -26,5 +26,25 @@ export const handlers = {
         ],
       })
     )
-  })
+  }),
+  graphql.query('GetUserIDs', (req, res, ctx) => {
+    return res(
+      ctx.data({
+        users: [
+          {
+            __typename: 'users',
+            id: 'aaaa1111-aa11-aa11-aa11-aaaaaa111111',
+          },
+          {
+            __typename: 'users',
+            id: 'bbbb1111-bb11-bb11-aa11-bbbbbb111111',
+          },
+          {
+            __typename: 'users',
+            id: 'cccc1111-cc11-cc11-cc11-ccccccc111111',
+          },
+        ],
+      })
+    )
+  }),
 }
