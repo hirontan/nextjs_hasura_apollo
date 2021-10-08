ca11994ccdaef3425b72daa3500ed7e56e6d41b5
hirontan <hirontan@sora.flights>
2021-10-08 23:08:33 +0900

    [Update] test_idがssg-nav以外をテストできるように修正

--- __tests__/NavBar.test.tsx
+++ __tests__/NavBar.test.tsx
@@ -39,16 +39,16 @@ describe('Navigation Test Cases', () => {
     userEvent.click(screen.getByTestId('fetchpolicy-nav'))
     expect(await screen.findByText('Hasura main page')).toBeInTheDocument()
 
+    userEvent.click(screen.getByTestId('crud-nav'))
+    expect(await screen.findByText('Hasura CRUD')).toBeInTheDocument()
     // テスト通らなかったので、後で修正
-    // userEvent.click(screen.getByTestId('crud-nav'))
-    // expect(await screen.findByText('Hasura CRUD')).toBeInTheDocument()
     // userEvent.click(screen.getByTestId('ssg-nav'))
     // expect(await screen.findByText('SSG+ISR')).toBeInTheDocument()
-    // userEvent.click(screen.getByTestId('memo-nav'))
-    // expect(
-    //   await screen.findByText('Custom Hook + useCallback + memo')
-    // ).toBeInTheDocument()
-    // userEvent.click(screen.getByTestId('home-nav'))
-    // expect(await screen.findByText('Next.js + GraphQL')).toBeInTheDocument()
+    userEvent.click(screen.getByTestId('memo-nav'))
+    expect(
+      await screen.findByText('Custom Hook + useCallback + memo')
+    ).toBeInTheDocument()
+    userEvent.click(screen.getByTestId('home-nav'))
+    expect(await screen.findByText('Next.js + GraphQL')).toBeInTheDocument()
   })
 })
