ecfaa34be53e73d9a1591e00797db78edfdf5b4e
hirontan <hirontan@sora.flights>
2021-10-09 11:48:50 +0900

    [Update] 作成時間が正しく表示されているかテスト

--- __tests__/HasuraCRUD.test.tsx
+++ __tests__/HasuraCRUD.test.tsx
@@ -27,5 +27,8 @@ describe('Hasura CRUD Test Cases', () => {
     render(page)
     expect(await screen.findByText('Hasura CRUD')).toBeInTheDocument()
     expect(await screen.findByText('Test user A')).toBeInTheDocument()
+    expect(
+      screen.getByText('2021-10-06T00:00:00.000000+00:00')
+    ).toBeInTheDocument()
   })
 })
